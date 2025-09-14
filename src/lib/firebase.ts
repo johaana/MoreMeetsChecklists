659ba0bd71859eee1a26d72636df6688a6c01a93
Aditi Khan <imiadu@gmail.com>
2025-09-14 10:21:37 +0000

    want to sign up using google

---
+++ src/lib/firebase.ts
@@ -0,0 +1,10 @@
+// This is a placeholder file. The Firebase config will be populated by the backend.
+// You can learn more about initializing Firebase here: https://firebase.google.com/docs/web/setup
+export const firebaseConfig = {
+  apiKey: "AIza-...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+};
