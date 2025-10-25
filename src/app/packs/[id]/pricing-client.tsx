f1ec05e5ff751b7cf8fcf71126adec886ca98b67
Aditi Khan <imiadu@gmail.com>
2025-10-25 21:36:14 +0000

    direct razorpay button please

--- src/app/packs/[id]/pricing-client.tsx
+++ src/app/packs/[id]/pricing-client.tsx
@@ -422,7 +422,9 @@ export default function PricingClient({ pack }: { pack: PremiumPack }) {
                                 </ul>
                             </CardContent>
                             <CardFooter className="mt-auto flex justify-center w-full">
-                               <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id={pack.paymentId} async> </script> </form>
+                               <form>
+                                   <script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id={pack.paymentId} async> </script> 
+                                </form>
                             </CardFooter>
                         </Card>
                     </div>
