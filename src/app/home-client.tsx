36c1b62d2d5c02838b4bf9de2d6854e234330dec
Aditi Khan <imiadu@gmail.com>
2025-11-10 14:18:06 +0000

    this?

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -77,7 +77,7 @@ const RefinedHeroSection = () => {
             {/* Mobile Content Block Wrapper */}
              <div className={cn(
               "container px-4 md:px-6 relative z-20 w-full h-full flex flex-col",
-              "md:justify-center justify-end pb-12"
+              "md:justify-center justify-end pb-20"
             )}>
               {/* Desktop Content */}
               <div className="hidden md:block max-w-2xl space-y-6">
