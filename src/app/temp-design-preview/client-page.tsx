3d976aee65bc9b0530ecee778525a6f168674e24
Aditi Khan <imiadu@gmail.com>
2025-11-26 09:30:20 +0000

    fix this as discussed please

--- src/app/temp-design-preview/client-page.tsx
+++ src/app/temp-design-preview/client-page.tsx
@@ -1,4 +1,7 @@
+
 // This page has been deleted and is no longer in use.
 export default function TempDesignClientPage() {
   return null;
 }
+
+    
--- src/app/temp-design-preview/page.tsx
+++ src/app/temp-design-preview/page.tsx
@@ -1,4 +1,7 @@
+
 // This page has been deleted and is no longer in use.
 export default function TempDesignPreviewPage() {
   return null;
 }
+
+    
