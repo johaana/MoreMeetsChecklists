1b702fe39da74f11682674ab4d4faf6fdcb59770
Aditi Khan <imiadu@gmail.com>
2025-12-24 19:23:41 +0000

    Error: In HTML, <html> cannot be a child of <body>. This will cause a hy

--- app/error.tsx
+++ app/error.tsx
@@ -7,10 +7,14 @@ export default function GlobalError({
 }) {
   return (
     <html>
-      <body style={{ padding: 20 }}>
-        <h1>Application Error</h1>
-        <pre>{error.message}</pre>
-        <pre>{error.stack}</pre>
+      <body>
+        <div style={{ padding: 20 }}>
+          <h1>Application Error</h1>
+          <p>We're sorry, something went wrong.</p>
+          <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-all', marginTop: '20px', background: '#f0f0f0', padding: '10px', borderRadius: '5px' }}>
+            {error.message}
+          </pre>
+        </div>
       </body>
     </html>
   );
