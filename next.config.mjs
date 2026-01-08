1cfc18cbc3e59214fe0fca3aed0258d23cb3b098
Aditi Khan <imiadu@gmail.com>
2026-01-08 21:07:19 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Switched to clie

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
